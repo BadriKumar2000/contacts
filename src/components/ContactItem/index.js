ff76cad7f975c56c778b32954ab577e7fb9389a8
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-21 15:59:55 +0530

    created components in src directory

---
+++ src/components/ContactItem/index.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import "./index.css";
+
+class ContactItem extends Component {
+  render() {
+    const { contactDetails } = this.props;
+    const { name, mobileNo, isFavorite } = contactDetails;
+
+    const starImgUrl = isFavorite
+      ? "https://assets.ccbp.in/frontend/react-js/star-filled-img.png"
+      : "https://assets.ccbp.in/frontend/react-js/star-outline-img.png";
+    return (
+      <li className="table-row">
+        <div className="table-cell name-column">
+          <p>{name}</p>
+        </div>
+        <hr className="separator" />
+        <div className="table-cell mobile-no-column">
+          <p className="mobile-no-value">{mobileNo}</p>
+          <button type="button" className="favorite-icon-container">
+            <img src={starImgUrl} className="favorite-icon" alt="star" />
+          </button>
+        </div>
+      </li>
+    );
+  }
+}
+
+export default ContactItem;
