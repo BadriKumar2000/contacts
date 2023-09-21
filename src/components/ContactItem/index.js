a06d70d9fc3ae04b1b6057e83e291ad7e6467695
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-21 16:35:53 +0530

    installed uuid and added onAddContact functionality

--- src/components/ContactItem/index.js
+++ src/components/ContactItem/index.js
@@ -2,9 +2,16 @@ import { Component } from "react";
 import "./index.css";
 
 class ContactItem extends Component {
+  onChangeFavoriteIcon = () => {
+    const { contactDetails, onChangeIcon } = this.props;
+    const { id } = contactDetails;
+    onChangeIcon(id);
+  };
+
   render() {
     const { contactDetails } = this.props;
-    const { name, mobileNo, isFavorite } = contactDetails;
+    const { name, mobileNo, isFavorite, id } = contactDetails;
+    console.log(id);
 
     const starImgUrl = isFavorite
       ? "https://assets.ccbp.in/frontend/react-js/star-filled-img.png"
@@ -17,7 +24,11 @@ class ContactItem extends Component {
         <hr className="separator" />
         <div className="table-cell mobile-no-column">
           <p className="mobile-no-value">{mobileNo}</p>
-          <button type="button" className="favorite-icon-container">
+          <button
+            type="button"
+            className="favorite-icon-container"
+            onClick={this.onChangeFavoriteIcon}
+          >
             <img src={starImgUrl} className="favorite-icon" alt="star" />
           </button>
         </div>
